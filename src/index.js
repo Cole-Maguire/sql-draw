87a666233febcb49f8a779003ec6cbbd8434fced
Cole <colemag383@gmail.com>
2020-06-13 22:09:47 +1200

    Support subqueries (though only the top level is currently shown)

--- src/index.js
+++ src/index.js
@@ -23,15 +23,39 @@ function updateFields() {
   mermaid.mermaidAPI.render('mermaid-graph-inner', sql, insertSvg);
 }
 
-function recurseColumns(object) {
+function isQueryLike(object) {
+  //determine if an object is it's own statement-ish thing (e.g. a subquery or a WHERE IN) that we need to treat seperately
+  return ["with", "type", "options", "distinct", "columns", "from", "where", "groupby", "having", "orderby", "limit"].every(i => i in object)
+}
+
+function recurseColumns(object, alias) {
   // recurse through the SQL AST for duck-typed column definitions
   // may have to add more cases if I get to subqueries/EXISTS etc.
   if (object !== null && object.table && object.column) {
-    return [`${object.table} : ${object.column}`]
+    // A standard column
+    return [`${object.table} : ${alias || object.column}`]
+
   } else if (Array.isArray(object)) {
-    return object.flatMap(recurseColumns);
+    //Array we can recurse through
+    return object.flatMap(i => recurseColumns(i)); //Having a shitton of nested flatMaps probably isn't effecient, but it is easy
+
   } else if (typeof object === 'object' && object !== null) {
-    return Object.values(object).flatMap(recurseColumns);
+      //There's a couple special cases of objects we need to deal with in regards to objects
+      const objectQueryLike = isQueryLike(object);
+
+      if (objectQueryLike && alias) {
+        //Looks like a subquery, as it's shaped like a statement, and it's got an AS clause/alias.
+        //Just grab the 'outputted' SELECTed columns
+        return object.columns.map(i => `${alias} : ${i.as || i.expr.column}`);
+      }
+      else if (objectQueryLike && !alias) {
+        // This is some sort of WHERE IN etc. where there is no aliased output. ignore.
+        return []
+      } else {
+        // Just a normal object. Continue recursing, though if we have an AS clause, start passing it down
+        return Object.values(object).flatMap(i => recurseColumns(i, alias || object.as));
+      }
+
   } else {
     return [];
   }
@@ -54,7 +78,7 @@ function parseSql(sql) {
   //tables
   let output = ast.from.map(i => `class ${i.as || i.table}`)
   // Columns
-  output = output.concat(recurseColumns(ast))
+  output = output.concat(recurseColumns([...Object.values(ast)]))
   // joins
   output = output.concat(ast.from.filter(i => i.join)
     .map(i => [i.on.left.table, JOIN_TYPES[i.join], i.on.right.table, ':', i.on.left.column, i.on.operator, i.on.right.column].join(' ')))
